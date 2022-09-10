ee5a180f17e8a1c0cdd8319cfd8d9ed9fee58b23
domaeyoshihiro <e8jbb@yahoo.co.jp>
2022-09-10 21:06:45 +0900

    todo-exercise

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,144 +16,102 @@
     width: 100vw;
   }
   h1 {
-    font-size:20px;
+    font-size: 25px;
     color: black;
-    padding: 20px;
+    padding-bottom: 20px;
   }
 
   .todolist {
     color: black;
     background-color: white;
     border-radius: 10px;
+    width: 50vw;
+    padding: 30px;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%,-50%);
   }
 
+  .upper__taskbox {
+    padding-bottom: 30px;
+    display: flex;
+    justify-content: space-between;
+  }
 
-  .upper_taskbox {
-  padding: 0 20px 20px;
-}
+  .task__text {
+    border: 1px solid	#DCDCDC;
+    border-radius: 3px;
+    width: 80%;
+    height: 30px;
+  }
 
-.task__text {
-  border: 1px solid	#DCDCDC;
-  border-radius: 3px;
-  width: 75%;
-  height: 30px;
-}
+  .create__btn {
+    border: 2px solid	#BA55D3;
+    border-radius: 3px;
+    background-color: white;
+    color: #BA55D3;
+    font-weight: bold;
+    padding: 8px 15px;
+    cursor: pointer;
+    transition: .3s;
+  }
 
-button.create_btn {
-  padding: 0 20px 0;
-}
+  .create__btn:hover {
+    background-color:	#BA55D3;
+    color: white;
+  }
 
+  table {
+    text-align: center;
+    width: 100%;
+  }
 
+  tr {
+    height: 50px;
+  }
 
-.create__btn {
-  border: 2px solid	#BA55D3;
-  border-radius: 3px;
-  background-color: white;
-  color: #BA55D3;
-  font-weight: bold;
-  padding: 8px 15px 8px;
-  margin-left: 30px;
-  cursor: pointer;
-  transition: .3s;
-}
+  .content__text {
+    border: 1px solid	#DCDCDC;
+    border-radius: 3px;
+    width: 90%;
+    height: 30px;
+  }
 
-.create__btn:hover {
-  background-color:	#BA55D3;
-  color: white;
-}
+  .update__btn {
+    border: 2px solid #FF6347;
+    border-radius: 3px;
+    background-color: white;
+    color: #FF6347;
+    font-weight: bold;
+    padding: 8px 15px;
+    cursor: pointer;
+    transition: .3s;
+  }
 
-.bottom_main {
-  margin: 10px 20px 20px;
-}
+  .update__btn:hover {
+    background-color: #FF6347;
+    color: white;
+  }
 
+  .delete__btn {
+    border: 2px solid #7FFFD4;
+    border-radius: 3px;
+    background-color: white;
+    color: #7FFFD4;
+    font-weight: bold;
+    padding: 8px 15px;
+    cursor: pointer;
+    transition: .3s;
+  }
 
-.created_at__th {
-  padding-bottom: 20px;
-  padding-right: 20px;
-  padding-left: 20px;
-}
-
-.task__th {
-  padding-bottom: 20px;
-  padding-right: 20px;
-}
-
-.update__th {
-  padding-bottom: 20px;
-  padding-right: 20px;
-}
-
-.delete__th {
-  padding-bottom: 20px;
-  padding-right: 20px;
-}
-
-.created_at__td {
-  padding-right: 20px;
-  padding-bottom: 10px;
-  padding-left: 20px;
-}
-
-.task__td {
-  padding-right: 20px;
-  padding-bottom: 10px;
-}
-
-.content__text {
-  border: 1px solid	#DCDCDC;
-  border-radius: 3px;
-  height: 30px;
-}
-
-.update__td {
-  padding-right: 20px;
-  padding-bottom: 10px;
-}
-
-.delete__td {
-  padding-right: 20px;
-  padding-bottom: 10px;
-}
-
-.update__btn {
-  border: 2px solid #FF6347;
-  border-radius: 3px;
-  background-color: white;
-  color: #FF6347;
-  font-weight: bold;
-  padding: 8px 15px 8px;
-  cursor: pointer;
-  transition: .3s;
-}
-
-.update__btn:hover {
-  background-color: #FF6347;
-  color: white;
-}
-
-.delete__btn {
-  border: 2px solid #7FFFD4;
-  border-radius: 3px;
-  background-color: white;
-  color: #7FFFD4;
-  font-weight: bold;
-  padding: 8px 15px 8px;
-  cursor: pointer;
-  transition: .3s;
-}
-
-.delete__btn:hover {
-  background-color:	#7FFFD4;
-  color: white;
-}
+  .delete__btn:hover {
+    background-color:	#7FFFD4;
+    color: white;
+  }
 </style>
 
 <body>
-<div class="todolist__relative">
   <div class="todolist">
     <div class="upper_main">
       <h1>Todo List</h1>
@@ -162,8 +120,8 @@ button.create_btn {
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
-        </ul>
-      <form action="/add" method="POST" class="upper_taskbox">
+          </ul>
+      <form action="/add" method="POST" class="upper__taskbox">
       @csrf
       <input type="text" name="content" class="task__text">
       <button type="submit" name="create-btn" class="create__btn">追加</button>
@@ -172,26 +130,26 @@ button.create_btn {
     <div class="bottom_main">
       <table>
         <tr>
-          <th class="created_at__th">作成日</th>
-          <th class="task__th">タスク名</th>
-          <th class="update__th">更新</th>
-          <th class="delete__th">削除</th>
+          <th>作成日</th>
+          <th>タスク名</th>
+          <th>更新</th>
+          <th>削除</th>
         </tr>
         @foreach ($todos as $todo)
         <tr>
-          <td class="created_at__td">
+          <td>
             {{$todo->created_at}}
           </td>
           <form action="/edit/{{ $todo->id }}" method="POST">
             @csrf
-          <td class="task__td">
+          <td>
             <input type="text" name="content" value="{{$todo->content}}" class="content__text">
           </td>
-          <td class="update__td">
+          <td>
             <button type="submit" name="update-btn" class="update__btn">更新</button>
           </td>
           </form>
-          <td class="delete__td"><form action="/delete/{{ $todo->id }}" method="POST">
+          <td><form action="/delete/{{ $todo->id }}" method="POST">
             @csrf
             <button type="submit" name="remove-btn" class="delete__btn">削除</button>
           </form>
@@ -199,7 +157,6 @@ button.create_btn {
         </tr>
         @endforeach
       </table>
-      </div>
     </div>
   </div>
 </div>
